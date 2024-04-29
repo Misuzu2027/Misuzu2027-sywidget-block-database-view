2e823492f93adcefe18fe5156a2059c925ee76a0
Misuzu2027 <154575405+Misuzu2027@users.noreply.github.com>
2024-04-29 19:44:38 +0800

    修改发布版本 v0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import HomeSvelte from "@/components/home.svelte";
-import "@/assets/icons/material/icon.ts"
 import "@/assets/icons/ant/icon.ts"
+import "@/assets/icons/material/icon.ts"
 import "./index.scss"
 
 let appElement = document.getElementById("app");
